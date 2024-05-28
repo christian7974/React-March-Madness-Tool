cd34b8663c456615b502956bf82b04ab0ccc8b41
christian7974 <ctropeano@uri.edu>
2024-05-28 15:41:10 -0400

    Added Preliminary Styling
    Added simple styling to the page.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 import { useEffect, useState } from 'react';
+
 import Autocomplete from "@mui/material/Autocomplete";
-import { TextField } from "@mui/material";
+import { TextField, createTheme, ThemeProvider } from "@mui/material";
+
+
+import Quicksand from './Quicksand/Quicksand-VariableFont_wght.ttf'
 import schoolNames from "./allTeams.js";
+import properStats from './allStats.js';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Quicksand",
+  }, overrides: { 
+    MuiCssBaseline: {
+      '@global': {
+        '@font-face': [Quicksand],
+      },
+    },
+  }
+
+});
 
 function App() {
-  // const [teamData, updateTeamData] = useState({});
   const [team1, updateTeam1] = useState("Rhode Island");
   const [team2, updateTeam2] = useState("Gonzaga");
 
@@ -31,51 +48,60 @@ function App() {
   }, [team1, team2]);
 
   return (
-    <div className="App">
-              <>
-                <Autocomplete
-                  disablePortal
-                  id="team1"
-                  options={schoolNames}
-                  value={team1}
-                  onChange={(event, newValue) => {
-                    updateTeam1(newValue);
-                  }}
-                  sx={{ width: 300, marginBottom: 10}}
-                  renderInput={(params) => <TextField {...params} label="Select Team 1" />}
-                />
-                <Autocomplete
-                  disablePortal
-                  id="team2"
-                  options={schoolNames}
-                  value={team2}
-                  onChange={(event, newValue) => {
-                    updateTeam2(newValue);
-                  }}
-                  sx={{ width: 300 }}
-                  renderInput={(params) => <TextField {...params} label="Select Team 2" />}
-                />
-                <h1>Team 1 is {team1}</h1>
-                <h1>Team 2 is {team2}</h1>
-                {loading ? (
-                  <p>Loading...</p>
-                ) : (
-                  <table style={{backgroundColor: 'red'}}>
-                    <tbody>
-                      {team1Data && team2Data && Object.keys(team1Data).map((key) => {
-                        return (
-                          <tr key={key}>
-                            <td>{key}</td>
-                            <td>{team1Data[key]}</td>
-                            <td>{team2Data[key]}</td>
-                          </tr>)
-                      })}
-                    </tbody>
-                  </table>
-                )}
-                
-              </>
-  </div>
+    <ThemeProvider theme={theme}>
+    <div className="App font-quicksand" style={{height: 'fit-content'}}>
+      <h1 className='flex mx-auto w-fit text-3xl'>March Madness Tool</h1>
+      <p className='flex mx-auto w-fit text-xl mt-4'>Select two teams to compare their statistics</p>
+        <div className='flex flex-col space-y-4 lg:space-x-4 sm:space-y-0 justify-between w-fit mx-auto mt-4 sm:flex-row sm:mt-10'>
+          <Autocomplete
+            disablePortal
+            id="team1"
+            options={schoolNames}
+            value={team1}
+            onChange={(event, newValue) => {
+              updateTeam1(newValue);
+            }}
+            sx={{ width: 300,}}
+            renderInput={(params) => <TextField {...params} label="Select Team 1" />}
+          />
+          <Autocomplete
+            disablePortal
+            id="team2"
+            options={schoolNames}
+            value={team2}
+            onChange={(event, newValue) => {
+              updateTeam2(newValue);
+            }}
+            sx={{ width: 300, fontFamily: 'Quicksand'}}
+            renderInput={(params) => <TextField {...params} label="Select Team 2" />}
+          />
+        </div>
+        {loading ? (
+          <p className='flex mx-auto w-fit text-3xl mt-5'>Loading...</p>
+        ) : (
+            <table className='bg-slate-50 mx-auto text-center my-2 text-lg font-semibold'>
+              <thead>
+                <tr className='text-slate-900 text-2xl bg-slate-400'>
+                  <th className='w-1/3'>Stat</th>
+                  <th className='w-1/3'>{team1}</th>
+                  <th className='w-1/3'>{team2}</th>
+                </tr>
+              </thead>
+              <tbody>
+                {team1Data && team2Data && Object.keys(properStats).slice(1).map((key) => {
+                  return (
+                    <tr key={key}>
+                      <td className='text-slate-900'>{properStats[key]}</td>
+                      <td style={{backgroundColor: team1Data[key] > team2Data[key] ? '#3CB371' : '#B22222'}}>{team1Data[key]}</td>
+                      <td style={{backgroundColor: team1Data[key] < team2Data[key] ? '#3CB371' : '#B22222'}}>{team2Data[key]}</td>
+                    </tr>)
+                })}
+              </tbody>
+            </table>
+        )}
+
+    </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/allStats.js
@@ -0,0 +1,36 @@
+const properStats = {
+    school_name: 'School Name',
+    wins: 'Wins',
+    losses: 'Losses',
+    win_loss_pct: 'Win-Loss Percentage',
+    srs: 'Simple Rating System',
+    sos: 'Strength of Schedule',
+    wins_conf: 'Wins in Conference',
+    losses_conf: 'Losses in Conference',
+    wins_home: 'Wins at Home',
+    losses_home: 'Losses at Home',
+    wins_visitor: 'Wins as Visitor',
+    losses_visitor: 'Losses as Visitor',
+    pts: 'Points',
+    opp_pts: 'Opponent Points',
+    mp: 'Minutes Played',
+    fg: 'Field Goals',
+    fga: 'Field Goal Attempts',
+    fg_pct: 'Field Goal Percentage',
+    fg3: 'Three-Point Field Goals',
+    fg3a: 'Three-Point Field Goal Attempts',
+    fg3_pct: 'Three-Point Field Goal Percentage',
+    ft: 'Free Throws',
+    fta: 'Free Throw Attempts',
+    ft_pct: 'Free Throw Percentage',
+    orb: 'Offensive Rebounds',
+    trb: 'Total Rebounds',
+    ast: 'Assists',
+    stl: 'Steals',
+    blk: 'Blocks',
+    tov: 'Turnovers',
+    pf: 'Personal Fouls'
+  };
+  
+  
+module.exports = properStats;
